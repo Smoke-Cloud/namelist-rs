077286eb91674cb04dcf511b5ad700b978c7b62c
Jake <joshannessy@Affinity-fire.com>
2022-05-01 10:51:32 +0100

    Fix tokenizer

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,7 +1,7 @@
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Span {
     lo: usize,
-    hi: usize,
+    len: usize,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -54,87 +54,208 @@ pub fn tokenize_nml(input: &str) -> Vec<LocatedToken> {
                             content.push(c);
                             state = TokenizerState::InQuote { start, content };
                         }
+                        '=' => {
+                            let token = Token::Equals;
+                            let span = Span { lo: i, len: 1 };
+                            tokens.push(LocatedToken { span, token });
+                            state = TokenizerState::Start;
+                        }
+                        '(' => {
+                            let token = Token::LeftBracket;
+                            let span = Span { lo: i, len: 1 };
+                            tokens.push(LocatedToken { span, token });
+                            state = TokenizerState::Start;
+                        }
+                        ')' => {
+                            let token = Token::RightBracket;
+                            let span = Span { lo: i, len: 1 };
+                            tokens.push(LocatedToken { span, token });
+                            state = TokenizerState::Start;
+                        }
+                        ':' => {
+                            let token = Token::Colon;
+                            let span = Span { lo: i, len: 1 };
+                            tokens.push(LocatedToken { span, token });
+                            state = TokenizerState::Start;
+                        }
+                        ',' => {
+                            let token = Token::Comma;
+                            let span = Span { lo: i, len: 1 };
+                            tokens.push(LocatedToken { span, token });
+                            state = TokenizerState::Start;
+                        }
                         _ => {
-                            let start = i;
-                            let mut content = String::new();
-                            content.push(c);
-                            state = TokenizerState::InIdentifier { start, content };
+                            if c.is_alphanumeric() {
+                                let start = i;
+                                let mut content = String::new();
+                                content.push(c);
+                                state = TokenizerState::InIdentifier { start, content };
+                            } else if c.is_alphanumeric() {
+                                let start = i;
+                                let mut content = String::new();
+                                content.push(c);
+                                state = TokenizerState::Whitespace { start, content };
+                            } else {
+                                panic!("{} is an invalid character", c);
+                            }
                         }
                     }
                 }
             }
-            TokenizerState::InQuote { start, mut content } => {
-                match c {
-                    '\'' => {
-                        content.push(c);
-                        let token = Token::QuotedStr(content);
-                        let span = Span { lo: start, hi: i };
-                        tokens.push(LocatedToken { span, token });
-                        state = TokenizerState::Start;
-                    }
-                    // '=' | ',' | '(' | ')' | ':' | '/' => {
-                    //     let previous = &input[start..i];
-                    //     // if !previous.is_empty() {
-                    //     //     tokens.push(previous);
-                    //     // }
-                    //     // tokens.push(&input[i..=i]);
-                    //     start = i + 1;
-                    // }
-                    _ => {
-                        content.push(c);
-                        state = TokenizerState::InQuote { start, content };
-                    }
+            TokenizerState::InQuote { start, mut content } => match c {
+                '\'' => {
+                    content.push(c);
+                    let len = content.len();
+                    let token = Token::QuotedStr(content);
+                    let span = Span { lo: start, len };
+                    tokens.push(LocatedToken { span, token });
+                    state = TokenizerState::Start;
                 }
-            }
+                _ => {
+                    content.push(c);
+                    state = TokenizerState::InQuote { start, content };
+                }
+            },
             TokenizerState::Whitespace { start, mut content } => {
                 if c.is_whitespace() {
                     content.push(c);
                     state = TokenizerState::Whitespace { start, content };
                 } else {
+                    let len = content.len();
+                    let token = Token::Whitespace(content);
+                    let span = Span { lo: start, len };
+                    tokens.push(LocatedToken { span, token });
                     match c {
                         '\'' => {
+                            let start = i;
                             let mut content = String::new();
                             content.push(c);
                             state = TokenizerState::InQuote { start, content };
                         }
+                        '=' => {
+                            let token = Token::Equals;
+                            let span = Span { lo: i, len: 1 };
+                            tokens.push(LocatedToken { span, token });
+                            state = TokenizerState::Start;
+                        }
+                        '(' => {
+                            let token = Token::LeftBracket;
+                            let span = Span { lo: i, len: 1 };
+                            tokens.push(LocatedToken { span, token });
+                            state = TokenizerState::Start;
+                        }
+                        ')' => {
+                            let token = Token::RightBracket;
+                            let span = Span { lo: i, len: 1 };
+                            tokens.push(LocatedToken { span, token });
+                            state = TokenizerState::Start;
+                        }
+                        ':' => {
+                            let token = Token::Colon;
+                            let span = Span { lo: i, len: 1 };
+                            tokens.push(LocatedToken { span, token });
+                            state = TokenizerState::Start;
+                        }
+                        ',' => {
+                            let token = Token::Comma;
+                            let span = Span { lo: i, len: 1 };
+                            tokens.push(LocatedToken { span, token });
+                            state = TokenizerState::Start;
+                        }
                         _ => {
-                            let mut content = String::new();
-                            content.push(c);
-                            state = TokenizerState::InIdentifier { start, content };
+                            if c.is_alphanumeric() {
+                                let start = i;
+                                let mut content = String::new();
+                                content.push(c);
+                                state = TokenizerState::InIdentifier { start, content };
+                            } else if c.is_alphanumeric() {
+                                let start = i;
+                                let mut content = String::new();
+                                content.push(c);
+                                state = TokenizerState::Whitespace { start, content };
+                            } else {
+                                panic!("{} is an invalid character", c);
+                            }
                         }
                     }
                 }
             }
             TokenizerState::InIdentifier { start, mut content } => {
-                if !c.is_whitespace() {
-                    content.push(c);
-                    state = TokenizerState::InIdentifier { start, content };
-                } else {
+                if c.is_whitespace() {
+                    let len = content.len();
                     let token = Token::Identifier(content);
-                    let span = Span { lo: start, hi: i };
+                    let span = Span { lo: start, len };
                     tokens.push(LocatedToken { span, token });
                     let start = i;
                     let mut content = String::new();
                     content.push(c);
                     state = TokenizerState::Whitespace { start, content };
+                } else if c.is_alphanumeric() || c == '_' {
+                    content.push(c);
+                    state = TokenizerState::InIdentifier { start, content };
+                } else {
+                    let len = content.len();
+                    let token = Token::Identifier(content);
+                    let span = Span { lo: start, len };
+                    tokens.push(LocatedToken { span, token });
+                    match c {
+                        '\'' => {
+                            let start = i;
+                            let mut content = String::new();
+                            content.push(c);
+                            state = TokenizerState::InQuote { start, content };
+                        }
+                        '=' => {
+                            let token = Token::Equals;
+                            let span = Span { lo: i, len: 1 };
+                            tokens.push(LocatedToken { span, token });
+                            state = TokenizerState::Start;
+                        }
+                        '(' => {
+                            let token = Token::LeftBracket;
+                            let span = Span { lo: i, len: 1 };
+                            tokens.push(LocatedToken { span, token });
+                            state = TokenizerState::Start;
+                        }
+                        ')' => {
+                            let token = Token::RightBracket;
+                            let span = Span { lo: i, len: 1 };
+                            tokens.push(LocatedToken { span, token });
+                            state = TokenizerState::Start;
+                        }
+                        ':' => {
+                            let token = Token::Colon;
+                            let span = Span { lo: i, len: 1 };
+                            tokens.push(LocatedToken { span, token });
+                            state = TokenizerState::Start;
+                        }
+                        ',' => {
+                            let token = Token::Comma;
+                            let span = Span { lo: i, len: 1 };
+                            tokens.push(LocatedToken { span, token });
+                            state = TokenizerState::Start;
+                        }
+                        _ => panic!("{} is an invalid character", c),
+                    }
                 }
             }
         }
     }
-    let i = if !input.is_empty() { input.len() - 1 } else { 0 };
     match state {
         TokenizerState::Start => {}
         TokenizerState::InQuote { .. } => {
             panic!("Unclosed quoted string")
         }
         TokenizerState::Whitespace { start, content } => {
+            let len = content.len();
             let token = Token::Whitespace(content);
-            let span = Span { lo: start, hi: i };
+            let span = Span { lo: start, len };
             tokens.push(LocatedToken { span, token });
         }
         TokenizerState::InIdentifier { start, content } => {
+            let len = content.len();
             let token = Token::Identifier(content);
-            let span = Span { lo: start, hi: i };
+            let span = Span { lo: start, len };
             tokens.push(LocatedToken { span, token });
         }
     }
@@ -150,23 +271,80 @@ mod tests {
         let s = "abc=2";
         let tokens = tokenize_nml(s);
         assert_eq!(
-            vec![LocatedToken {
-                span: Span { lo: 0, hi: 4 },
-                token: Token::Identifier("abc=2".to_string()),
-            }],
+            vec![
+                LocatedToken {
+                    span: Span { lo: 0, len: 3 },
+                    token: Token::Identifier("abc".to_string()),
+                },
+                LocatedToken {
+                    span: Span { lo: 3, len: 1 },
+                    token: Token::Equals,
+                },
+                LocatedToken {
+                    span: Span { lo: 4, len: 1 },
+                    token: Token::Identifier("2".to_string()),
+                }
+            ],
             tokens
         );
     }
 
-    // #[test]
-    // fn simple_tokens1() {
-    //     let s = "abc=2,'ad c' (2,:)";
-    //     let tokens = tokenize_nml(s);
-    //     assert_eq!(
-    //         vec!["abc", "=", "2", ",", "'ad c'", "(", "2", ",", ":", ")"],
-    //         tokens
-    //     );
-    // }
+    #[test]
+    fn simple_tokens1() {
+        let s = "abc=2,'ad c' (2,:)";
+        let tokens = tokenize_nml(s);
+        let expected = vec![
+            LocatedToken {
+                span: Span { lo: 0, len: 3 },
+                token: Token::Identifier("abc".to_string()),
+            },
+            LocatedToken {
+                span: Span { lo: 3, len: 1 },
+                token: Token::Equals,
+            },
+            LocatedToken {
+                span: Span { lo: 4, len: 1 },
+                token: Token::Identifier("2".to_string()),
+            },
+            LocatedToken {
+                span: Span { lo: 5, len: 1 },
+                token: Token::Comma,
+            },
+            LocatedToken {
+                span: Span { lo: 6, len: 6 },
+                token: Token::QuotedStr("'ad c'".to_string()),
+            },
+            LocatedToken {
+                span: Span { lo: 12, len: 1 },
+                token: Token::Whitespace(" ".to_string()),
+            },
+            LocatedToken {
+                span: Span { lo: 13, len: 1 },
+                token: Token::LeftBracket,
+            },
+            LocatedToken {
+                span: Span { lo: 14, len: 1 },
+                token: Token::Identifier("2".to_string()),
+            },
+            LocatedToken {
+                span: Span { lo: 15, len: 1 },
+                token: Token::Comma,
+            },
+            LocatedToken {
+                span: Span { lo: 16, len: 1 },
+                token: Token::Colon,
+            },
+            LocatedToken {
+                span: Span { lo: 17, len: 1 },
+                token: Token::RightBracket,
+            },
+        ];
+        // assert_eq!(expected, tokens);
+        // assert_eq!(expected.len(), tokens.len());
+        for (expected, found) in expected.into_iter().zip(tokens.into_iter()) {
+            assert_eq!(expected, found);
+        }
+    }
 
     // #[test]
     // fn simple_tokens2() {
